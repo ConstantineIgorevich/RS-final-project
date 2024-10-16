672edebe61b56b0a8851f8e1449c94440ba64cda
Davydovich <kostiarabota@gmail.com>
2024-10-16 17:08:14 +0300

    copy repo

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import './app/sass/reset.scss';
+import './app/sass/style.scss';
+
+import App from './app/app';
+
+const app = new App(document.body);
+
+app.start();
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const prodConfig = require('./webpack.prod.config');
+const devConfig = require('./webpack.dev.config');
+const { merge } = require('webpack-merge');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(s[ac]ss|css)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|png|svg|gif|mp3)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+    }),
+    new Dotenv(),
+    new FaviconsWebpackPlugin('src/app/assets/images/favicon.ico'),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+  },
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? prodConfig : devConfig;
+
+  return merge(baseConfig, envConfig);
+};
